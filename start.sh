a0ae51ff0824563024bc9dee042dafb6cf82a753
ハックモンカー <46002790+hackmonker@users.noreply.github.com>
2023-10-10 01:30:01 +0530

    test

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Replace placeholders in the config.json with actual environment variables
+# Replace placeholders in the config.example.json with actual environment variables
 envsubst < /telminal/config.example.json > /usr/local/lib/python3.9/site-packages/telminal/config.json
 
 # Start your application with the updated configuration
